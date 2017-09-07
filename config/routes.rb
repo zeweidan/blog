c7c988b135aefbcdb351ec0b5bc40bd3e0d3a288
zeweidan <zewei.dan.15@ucl.ac.uk>
2017-09-07 17:22:50 +0800

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :articles do
   	resources :comments
   end
+
+  resources :contact
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
